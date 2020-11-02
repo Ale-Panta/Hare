99f90cfd0b77cd8590282063a003c8f367487e19
AlessandroPantano <alessandropantano74@gmail.com>
2020-11-02 15:13:52 +0100

    Add premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Hare"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hare"
+	location "Hare"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir  ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir  ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HR_PLATFORM_WINDOWS",
+			"HR_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "HR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HR_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir  ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir  ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Hare/vendor/spdlog/include",
+		"Hare/src"
+	}
+
+	links
+	{
+		"Hare"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HR_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "HR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HR_DIST"
+		optimize "On"
