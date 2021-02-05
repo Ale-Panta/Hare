d6606920d4177d6b14d5bc09822ff9435bd38ac2
alessandro pantano <alessandropantano74@gmail.com>
2021-02-05 16:21:03 +0100

    Implement Component / Prperties Panel

--- Hare-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Hare-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -3,6 +3,7 @@
 #include "Hare/Scene/Components.h"
 
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Hare
 {
@@ -19,12 +20,25 @@ namespace Hare
 
 	void SceneHierarchyPanel::OnImGuiRender()
 	{
+		// Hierarchy...
 		ImGui::Begin("Scene Hierarchy");
 		m_Context->m_Registry.each([&](auto entityID)
 			{
 				Entity entity = Entity(entityID, m_Context.get());
 				DrawEntityNode(entity);
 			});
+		ImGui::End();
+
+		// Deselection...
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
+		// Components...
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+		{
+			DrawComponents(m_SelectionContext);
+		}
 		ImGui::End();
 	}
 
@@ -43,6 +57,34 @@ namespace Hare
 		if (opened)
 		{
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 
