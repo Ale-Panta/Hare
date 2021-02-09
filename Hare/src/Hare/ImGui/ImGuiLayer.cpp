55931b8c70825222434aa5e32aab3220583a9563
alessandro pantano <alessandropantano74@gmail.com>
2021-02-09 17:53:04 +0100

    Styling editor

--- Hare/src/Hare/ImGui/ImGuiLayer.cpp
+++ Hare/src/Hare/ImGui/ImGuiLayer.cpp
@@ -113,34 +113,46 @@ namespace Hare
 	{
 		auto& colors = GetStyle().Colors;
 
-		colors[ImGuiCol_WindowBg] = ImVec4(0.1f, 0.105f, 0.11f, 1.0f);
+		colors[ImGuiCol_WindowBg]			= ImVec4(0.178f, 0.178f, 0.178f, 1.0f);
 
-		// Headers
-		colors[ImGuiCol_Header] = ImVec4(0.2f, 0.205f, 0.21f, 1.0f);
-		colors[ImGuiCol_HeaderHovered] = ImVec4(1.0f, 0.643f, 0.434f, 1.0f);
-		colors[ImGuiCol_HeaderActive] = ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
+		// --- Headers ------------------------------------------------------------
+		colors[ImGuiCol_Header]				= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+		colors[ImGuiCol_HeaderHovered]		= ImVec4(0.378f, 0.307f, 0.409f, 1.0f);
+		colors[ImGuiCol_HeaderActive]		= ImVec4(0.150f, 0.150f, 0.150f, 1.0f);
 
-		// Buttons
-		colors[ImGuiCol_Button] = ImVec4(0.2f, 0.205f, 0.21f, 1.0f);
-		colors[ImGuiCol_ButtonHovered] = ImVec4(0.3f, 0.305f, 0.31f, 1.0f);
-		colors[ImGuiCol_ButtonActive] = ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
+		// --- Buttons ------------------------------------------------------------
+		colors[ImGuiCol_Button]				= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+		colors[ImGuiCol_ButtonHovered]		= ImVec4(0.378f, 0.307f, 0.409f, 1.0f);
+		colors[ImGuiCol_ButtonActive]		= ImVec4(0.150f, 0.150f, 0.150f, 1.0f);
 
-		// Frames Bg
-		colors[ImGuiCol_FrameBg] = ImVec4(0.2f, 0.205f, 0.21f, 1.0f);
-		colors[ImGuiCol_FrameBgHovered] = ImVec4(0.3f, 0.305f, 0.31f, 1.0f);
-		colors[ImGuiCol_FrameBgActive] = ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
+		// --- Frame BG -----------------------------------------------------------
+		colors[ImGuiCol_FrameBg]			= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+		colors[ImGuiCol_FrameBgHovered]		= ImVec4(0.378f, 0.307f, 0.409f, 1.0f);
+		colors[ImGuiCol_FrameBgActive]		= ImVec4(0.150f, 0.150f, 0.150f, 1.0f);
 
-		// Tabs
-		colors[ImGuiCol_Tab] = ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-		colors[ImGuiCol_TabHovered] = ImVec4(0.38f, 0.3885f, 0.381f, 1.0f);
-		colors[ImGuiCol_TabActive] = ImVec4(0.28f, 0.2885f, 0.281f, 1.0f);
-		colors[ImGuiCol_TabUnfocused] = ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-		colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.2f, 0.205f, 0.21f, 1.0f);
+		// --- Tabs ---------------------------------------------------------------
+		colors[ImGuiCol_Tab]				= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+		colors[ImGuiCol_TabHovered]			= ImVec4(0.378f, 0.307f, 0.409f, 1.0f);
+		colors[ImGuiCol_TabActive]			= ImVec4(0.178f, 0.178f, 0.178f, 1.0f);
+		colors[ImGuiCol_TabUnfocused]		= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
 
-		// Title
-		colors[ImGuiCol_TitleBg] = ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-		colors[ImGuiCol_TitleBgActive] = ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-		colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.95f, 0.1585f, 0.951f, 1.0f);
+		// --- Titles --------------------------------------------------------------
+		colors[ImGuiCol_TitleBg]			= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+		colors[ImGuiCol_TitleBgActive]		= ImVec4(0.378f, 0.307f, 0.409f, 1.0f);
+		colors[ImGuiCol_TitleBgCollapsed]	= ImVec4(0.378f, 0.307f, 0.409f, 1.0f);
+
+		// --- PopUp ---------------------------------------------------------------
+		colors[ImGuiCol_PopupBg]			= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+
+		// --- Border --------------------------------------------------------------
+		colors[ImGuiCol_Border]				= ImVec4(0.378f, 0.307f, 0.409f, 1.0f);
+
+		// --- Menu Bar ------------------------------------------------------------
+		colors[ImGuiCol_MenuBarBg]			= ImVec4(0.139f, 0.139f, 0.139f, 1.0f);
+
+		// --- Text ----------------------------------------------------------------
+		colors[ImGuiCol_TextSelectedBg]		= ImVec4(0.178f, 0.178f, 0.178f, 1.0f);
 	}
 
 }
