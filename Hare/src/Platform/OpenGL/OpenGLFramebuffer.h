94dc30b675363fd28a584e44caf303069f517152
alessandro pantano <alessandropantano74@gmail.com>
2021-02-13 02:22:33 +0100

    Added framebuffer API to clear texture attachment

--- Hare/src/Platform/OpenGL/OpenGLFramebuffer.h
+++ Hare/src/Platform/OpenGL/OpenGLFramebuffer.h
@@ -16,6 +16,7 @@ namespace Hare
 		virtual void Unbind() override;
 		virtual void Resize(uint32_t width, uint32_t height) override;
 		virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
+		virtual void ClearAttachment(uint32_t attachmentIndex, int value) override;
 		inline virtual uint32_t GetColorAttachmentRenderID(uint32_t index = 0) const override { HR_CORE_ASSERT(index < m_ColorAttachments.size()); return m_ColorAttachments[index]; }
 		inline virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
 
