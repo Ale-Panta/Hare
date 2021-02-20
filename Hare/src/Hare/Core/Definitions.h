0cd2f7b2732626489743b2eee51646d3f559be90
alessandro pantano <alessandropantano74@gmail.com>
2021-02-20 11:53:18 +0100

    Declare integers types

--- Hare/src/Hare/Core/Definitions.h
+++ Hare/src/Hare/Core/Definitions.h
@@ -1,3 +1,25 @@
 #pragma once
+
+// In this file are declared all types used inside the engine and client project. When it will be compiled, the right
+// variable type is defined per platform. 
+
+// --- boolean --------------------------------------------------------------------------------------------------------
+
 #define HR_FALSE 0
 #define HR_TRUE 1
+
+// --- Integers -------------------------------------------------------------------------------------------------------
+
+#define HR_INT int
+#define HR_INT8 int8
+#define HR_INT16 int16
+#define HR_INT32 int32
+#define HR_INT64 int64
+#define HR_INT8 uint8_t
+#define HR_INT16 uint16_t
+#define HR_INT32 uint32_t
+#define HR_INT64 uint64_t
+
+// --- Floats -------------------------------------------------------------------------------------------------------
+
+#define HR_FLOAT float
